5778e9059d74d21432a4b9a1e0d3a3fabbee66e3
Rais Garifzyanov <garifzyanov.rr@gmail.com>
2022-02-13 12:41:37 +0300

    copy to new repository

---
+++ app/src/main/java/ru/rayanis/issuedproducts/data/Product.kt
@@ -0,0 +1,17 @@
+package ru.rayanis.issuedproducts.data
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+import java.util.*
+
+
+data class Product(
+    var title: String = "" ,
+    val destination: String = "" ,
+    val date: String ,
+    val quantity: String ,
+    val productCost: String ,
+    val description: String = "" ,
+    val quantPersons: String ,
+//    val list: MutableList<UsedMaterial>
+)
